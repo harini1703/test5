84e2c9110dd0376d57d1a3366901bd05217a0261
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2024-04-23 10:01:28 +0530

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -5,3 +5,38 @@ import streamlit as st
 pickle_in = open('class.pkl', 'rb')
 clf = pickle.load(pickle_in)
 st.title("Windy Predict")
+b=st.selectbox("SELECT THE Wind Direction:",('Calm','NNW','NE','NW','WNW','NNE','ENE','N','Variable','E','SE','W','SW','S','ESE','SSE','WSW'))
+if b=='Calm':
+  b=0
+elif b=='NNW':
+  b=34
+elif b=='NE':
+  a=5
+elif b=='NW':
+  b=32
+elif b=='WNW':
+  b=29
+elif b=='NNE':
+  b=2
+elif b=='ENE':
+  b=7 
+elif b=='N':
+  b=36
+elif b=='Variable':
+  b=99
+elif b=='E':
+  b=9
+elif b=='SE':
+  b=14
+elif b=='W':
+  b=27
+elif b=='SW':
+  b=23
+elif b=='S':
+  b=18
+elif b=='ESE':
+  b=11
+elif b=='SSE':
+  b=16
+else:
+  a=25
